484e2a48b1709bb139eb975f38c357294e9f9ada
Eric Ross <epr@rossmbox.com>
2024-10-21 09:54:54 -0700

    Initial changes

--- Chapter 05/Beginning of Chapter/webapp/src/server.ts
+++ Chapter 05/Beginning of Chapter/webapp/src/server.ts
@@ -1,21 +1,9 @@
 import { createServer } from "http";
 import { handler } from "./handler";
-
-const port = 5000;
-
+const port = 3000;
 const server = createServer();
-
-server.on("request", (req, res) => {
-    if (req.url?.endsWith("favicon.ico")) {
-        res.statusCode = 404;
-        res.end();
-    } else {
-        handler(req, res)
-    }
-});
-
+server.on("request", handler);
 server.listen(port);
-
 server.on("listening", () => {
     console.log(`(Event) Server listening on port ${port}`);
 });
