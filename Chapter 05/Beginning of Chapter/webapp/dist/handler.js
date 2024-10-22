11ce744c17640b6aa3c9841d33a86bcfba497d56
Eric Ross <epr@rossmbox.com>
2024-10-22 11:13:27 -0700

    Add keyword

--- Chapter 05/Beginning of Chapter/webapp/dist/handler.js
+++ Chapter 05/Beginning of Chapter/webapp/dist/handler.js
@@ -1,20 +1,24 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.handler = void 0;
+const url_1 = require("url");
 const handler = async (req, resp) => {
-    console.log(`user-agent: ${req.headers["user-agent"]}`);
-    console.log(`---- HTTP Method: ${req.method}, URL: ${req.url}`);
-    const parsedURL = new URL(req.url ?? "", `http://${req.headers.host}`);
-    console.log(`protocol: ${parsedURL.protocol}`);
-    console.log(`hostname: ${parsedURL.hostname}`);
-    console.log(`port: ${parsedURL.port}`);
-    console.log(`pathname: ${parsedURL.pathname}`);
-    parsedURL.searchParams.forEach((val, key) => {
-        console.log(`Search param: ${key}: ${val}`);
-    });
-    console.log(parsedURL);
-    //resp.writeHead(404,'Missing data');
-    resp.write('Hello there.\n');
-    resp.end("Hello, World");
+    const parsedURL = new url_1.URL(req.url ?? "", `http://${req.headers.host}`);
+    if (req.method !== "GET" || parsedURL.pathname == "/favicon.ico") {
+        resp.writeHead(404, "Not Found");
+        resp.end();
+        return;
+    }
+    else {
+        resp.writeHead(200, "OK");
+        if (!parsedURL.searchParams.has("keyword")) {
+            resp.write("Hello, HTTPx");
+        }
+        else {
+            resp.write(`Hello, ${parsedURL.searchParams.get("keyword")}`);
+        }
+        resp.end();
+        return;
+    }
 };
 exports.handler = handler;
--- Chapter 05/Beginning of Chapter/webapp/src/handler.ts
+++ Chapter 05/Beginning of Chapter/webapp/src/handler.ts
@@ -1,17 +1,19 @@
 import { IncomingMessage, ServerResponse } from "http";
+import { URL } from "url";
 export const handler = async (req: IncomingMessage, resp: ServerResponse) => {
-    console.log(`user-agent: ${req.headers["user-agent"]}`);
-    console.log(`---- HTTP Method: ${req.method}, URL: ${req.url}`);
     const parsedURL = new URL(req.url ?? "", `http://${req.headers.host}`);
-    console.log(`protocol: ${parsedURL.protocol}`);
-    console.log(`hostname: ${parsedURL.hostname}`);
-    console.log(`port: ${parsedURL.port}`);
-    console.log(`pathname: ${parsedURL.pathname}`);
-    parsedURL.searchParams.forEach((val, key) => {
-        console.log(`Search param: ${key}: ${val}`)
-    });
-    console.log(parsedURL);
-    //resp.writeHead(404,'Missing data');
-    resp.write('Hello there.\n')
-    resp.end("Hello, World");
+    if (req.method !== "GET" || parsedURL.pathname == "/favicon.ico") {
+        resp.writeHead(404, "Not Found");
+        resp.end();
+        return;
+    } else {
+        resp.writeHead(200, "OK");
+        if (!parsedURL.searchParams.has("keyword")) {
+            resp.write("Hello, HTTPx");
+        } else {
+            resp.write(`Hello, ${parsedURL.searchParams.get("keyword")}`);
+        }
+        resp.end();
+        return;       
+    }
 };
