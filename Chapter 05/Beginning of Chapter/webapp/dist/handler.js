ea3231593c4f449f4aab63cd67495ef9d974c841
Eric Ross <epr@rossmbox.com>
2024-10-21 20:40:47 -0700

    response methods

--- Chapter 05/Beginning of Chapter/webapp/dist/handler.js
+++ Chapter 05/Beginning of Chapter/webapp/dist/handler.js
@@ -2,7 +2,19 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.handler = void 0;
 const handler = async (req, resp) => {
-    console.log(req);
+    console.log(`user-agent: ${req.headers["user-agent"]}`);
+    console.log(`---- HTTP Method: ${req.method}, URL: ${req.url}`);
+    const parsedURL = new URL(req.url ?? "", `http://${req.headers.host}`);
+    console.log(`protocol: ${parsedURL.protocol}`);
+    console.log(`hostname: ${parsedURL.hostname}`);
+    console.log(`port: ${parsedURL.port}`);
+    console.log(`pathname: ${parsedURL.pathname}`);
+    parsedURL.searchParams.forEach((val, key) => {
+        console.log(`Search param: ${key}: ${val}`);
+    });
+    console.log(parsedURL);
+    //resp.writeHead(404,'Missing data');
+    resp.write('Hello there.\n');
     resp.end("Hello, World");
 };
 exports.handler = handler;
--- Chapter 05/Beginning of Chapter/webapp/src/handler.ts
+++ Chapter 05/Beginning of Chapter/webapp/src/handler.ts
@@ -1,5 +1,17 @@
 import { IncomingMessage, ServerResponse } from "http";
 export const handler = async (req: IncomingMessage, resp: ServerResponse) => {
-    console.log(req);
+    console.log(`user-agent: ${req.headers["user-agent"]}`);
+    console.log(`---- HTTP Method: ${req.method}, URL: ${req.url}`);
+    const parsedURL = new URL(req.url ?? "", `http://${req.headers.host}`);
+    console.log(`protocol: ${parsedURL.protocol}`);
+    console.log(`hostname: ${parsedURL.hostname}`);
+    console.log(`port: ${parsedURL.port}`);
+    console.log(`pathname: ${parsedURL.pathname}`);
+    parsedURL.searchParams.forEach((val, key) => {
+        console.log(`Search param: ${key}: ${val}`)
+    });
+    console.log(parsedURL);
+    //resp.writeHead(404,'Missing data');
+    resp.write('Hello there.\n')
     resp.end("Hello, World");
 };
